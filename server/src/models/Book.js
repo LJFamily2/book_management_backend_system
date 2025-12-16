add4e42a70a81df54f249487292e8cd1f313483b
LJFamily2 <temp@example.com>
2025-12-16 15:30:37 +0700

    feat: add Book and User models with validation schemas

---
+++ server/src/models/Book.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const bookSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    summary: {
+      type: String,
+    },
+    publicationYear: {
+      type: Number,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Book", bookSchema);
---
+++ server/src/models/User.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    phone: {
+      type: String,
+    },
+    nationality: {
+      type: String,
+    },
+    birthday: {
+      type: Date,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["ADMIN", "STUDENT"],
+      default: "STUDENT",
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
